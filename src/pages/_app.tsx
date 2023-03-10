05ca701047316066ce428194d37416639b6f35ff
Anzo256 <anzoangelo@gmail.com>
2023-03-10 13:55:49 -0800

    Creating the Layout,Navbar in the components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,17 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import {theme} from '../chakra/theme'
+import Layout from '../components/Layout/Layout';
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-    <Component {...pageProps} />
+      <Layout>
+      <Component {...pageProps} />
+      </Layout>
+    
    </ChakraProvider>
   );
 }
